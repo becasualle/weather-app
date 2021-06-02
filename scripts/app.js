b7eac77649b940afa5b3f7fe38f6887ae6e6a29e
Mark <mark.nelubin@gmail.com>
2021-06-02 17:21:18 +0300

    refactored using OOP Classes. Put everything about weather requests to Forecast class

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = data => {
 
@@ -33,15 +34,6 @@ const updateUI = data => {
 
 };
 
-const updateCity = async (city) => {
-
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-    
-    return {cityDets, weather};
-
-};
-
 cityForm.addEventListener('submit', e=>{
     // prevent default action
     e.preventDefault();
@@ -51,7 +43,7 @@ cityForm.addEventListener('submit', e=>{
     cityForm.reset();
 
     // update the ui with new city
-    updateCity(city)
+    forecast.updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 
@@ -60,7 +52,7 @@ cityForm.addEventListener('submit', e=>{
 })
 
 if(localStorage.city){
-    updateCity(localStorage.city)
+    forecast.updateCity(localStorage.city)
         .then(data => updateUI(data))
         .catch(err => console.log(err))
 }
